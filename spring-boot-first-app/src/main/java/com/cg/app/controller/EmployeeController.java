fdd4192ce3e35f3eb07c18023a8ebeba4e1f1650
Ramanuj Das <ramanujds9@gmail.com>
2021-10-13 17:56:20 +0530

    Added Model Attribute

--- spring-boot-first-app/src/main/java/com/cg/app/controller/EmployeeController.java
+++ spring-boot-first-app/src/main/java/com/cg/app/controller/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.cg.app.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cg.app.model.Employee;
@@ -9,18 +13,28 @@ import com.cg.app.model.Employee;
 @Controller
 public class EmployeeController {
 
-	@GetMapping("/employee")
-	public ModelAndView getEmployeeDetails() {
-		
-		Employee emp=new Employee(10001,"Rahul Dutta","rahul@gmail.com");
-		ModelAndView mv=new ModelAndView();
-		mv.setViewName("show.jsp");
-		mv.addObject("emp",emp);
-		return mv;
-		
-		
-	}
 	
+
+//	@GetMapping("/employee")
+//	public ModelAndView getEmployeeDetails(@RequestParam("empidtxt") int id, 
+//											@RequestParam("empnametxt") String name,
+//											@RequestParam("emailtxt") String email) {
+//		
+//		Employee emp=new Employee(id,name,email);
+//		
+//		ModelAndView mv=new ModelAndView();
+//		mv.setViewName("show.jsp");
+//		mv.addObject("emp",emp);
+//		return mv;
+//		
+//		
+//	}
+	
+	
+	@GetMapping("/employee")
+	public String getEmployeeDetails(@ModelAttribute("emp") Employee emp) {
+		return "show";
+	}
 	
 	
 }
---
+++ spring-boot-first-app/src/main/webapp/employee-form.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello World</title>
+</head>
+<body>
+<form action="employee">
+<input type="number" name="employeeId">
+<input type="text" name="employeeName">
+<input type="text" name="email">
+<button type="submit">Enter Details</button>
+</form>
+</body>
+</html>
